a5a61a1c2ab39c910393760578ef3b967152acb4
ninjaflesh <49476875+ninjaflesh@users.noreply.github.com>
2022-09-25 21:22:35 +0300

    dz 2

--- Circle/circle.h
+++ Circle/circle.h
@@ -1,9 +1,17 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
 class circle
 {
 public:
 	void get();
 	void show();
+	circle();
+	circle(double, double, double);
+	circle(const circle&);
+	~circle();
 
 private:
 	double x, y, r;
