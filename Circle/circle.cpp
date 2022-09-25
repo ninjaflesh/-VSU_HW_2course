16da3a7b2ce7f7ceda10becdb6b3d16fc89c913c
ninjaflesh <49476875+ninjaflesh@users.noreply.github.com>
2022-09-25 21:22:22 +0300

    dz 2

--- Circle/circle.cpp
+++ Circle/circle.cpp
@@ -1,21 +1,36 @@
-#include <iostream>
 #include "D:\cpp\Circle\Circle\circle.h"
-
-using namespace std;
 
 void circle::get() {
 	cout << "Введите коордитнаты центра окружности (x, y): ";
 	cin >> x >> y;
-	
+
 	bool f = true;
 	while (f) {
 		cout << "Введите радиус окружности: ";
 		cin >> r;
 
-		if (r >= 1) f = false;
+		if (r > 0) f = false;
 	}
 	cout << endl;
 }
 void circle::show() {
-	cout << "(x-" << x << ")^2 + (y-" << y << ")^2 = " << pow(r, 2) << endl;
+	if (x < 0)
+		cout << "(x+" << abs(x) << ")^2 + ";
+	else if (x == 0)
+		cout << "x^2 + ";
+	else
+		cout << "(x-" << x << ")^2 + ";
+
+	if (y < 0)
+		cout << "(y+" << abs(y) << ")^2 = ";
+	else if (y == 0)
+		cout << "y^2 = ";
+	else
+		cout << "(y-" << y << ")^2 = ";
+
+	cout << pow(r, 2) << endl;
 }
+circle::circle() {}
+circle::circle(const circle& a) :x(a.x), y(a.y), r(a.r) {}
+circle::circle(double a, double b, double c) : x(a), y(b), r(c) {}
+circle::~circle() {}
