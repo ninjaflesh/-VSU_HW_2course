81287bb7833cf9e956cc0ae4d255749564c5a6e5
ninjaflesh <49476875+ninjaflesh@users.noreply.github.com>
2022-10-16 16:00:46 +0300

    add in/out operator, file load

--- Circle/main.cpp
+++ Circle/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "D:\cpp\Circle\Circle\circle.h"
 
 using namespace std;
@@ -6,12 +8,31 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "rus");
 
-	circle a, b(0, -1, 5);
-	a.get();
-	a.show();
-	b.show();
-	cout << (a == b) << endl;
-	cout << "Площадь окружности: " << circle_area(a) << endl;
+	fstream f("source.txt", ios_base::in);
+
+	char str[100];
+	int k = 0;
+	while (!f.eof()) {
+		f.getline(str, 100);
+		k++;
+	}
+	f.close();
+
+	circle* temp = new circle[k];
+	f.open("source.txt", ios_base::in);
+	int i = 0;
+	while (!f.eof()) {
+		f >> temp[i];
+		i++;
+	}
+	f.close();
+
+	f.open("source.txt", ios_base::app);
+	f << endl;
+	for (int i = 0; i < k; i++) {
+		f << circle_area(temp[i]) << endl;
+	}
+	f.close();
 
 	return 0;
 }
