ed18da46aef9821cb232b1e6c165f4c9563cb06c
ninjaflesh <49476875+ninjaflesh@users.noreply.github.com>
2022-11-03 12:52:51 +0300

    .

--- Circle/main.cpp
+++ Circle/main.cpp
@@ -9,6 +9,32 @@ using namespace std;
 int main() {
 	setlocale(LC_ALL, "rus");
 
+	//fstream f("source.txt", ios_base::in);
+
+	//char str[100];
+	//int k = 0;
+	//while (!f.eof()) {
+	//	f.getline(str, 100);
+	//	k++;
+	//}
+	//f.close();
+
+	//circle* temp = new circle[k];
+	//f.open("source.txt", ios_base::in);
+	//int i = 0;
+	//while (!f.eof()) {
+	//	f >> temp[i];
+	//	i++;
+	//}
+	//f.close();
+
+	//f.open("source.txt", ios_base::app);
+	//f << endl;
+	//for (int i = 0; i < k; i++) {
+	//	f << circle_area(temp[i]) << endl;
+	//}
+	//f.close();
+
 	angem_circle a, b(1, 4, -3, 5), c(b);
 	a.get();
 	a.show();
