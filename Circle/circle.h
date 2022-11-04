8ae7b87aab6d767db0a2b97f7603eb9f01562a28
ninjaflesh <49476875+ninjaflesh@users.noreply.github.com>
2022-11-04 12:41:48 +0300

    .

--- Circle/circle.h
+++ Circle/circle.h
@@ -7,7 +7,12 @@ class circle {
 public:
 	void get();
 	void show();
+	circle operator++(int);
+	circle operator--(int);
+	circle& operator=(const circle&);
 	bool operator==(circle);
+	friend bool operator>(circle, circle);
+	friend bool operator<(circle, circle);
 	friend ostream& operator<<(ostream&, const circle);
 	friend istream& operator>>(istream&, circle&);
 	friend double circle_area(circle x);
